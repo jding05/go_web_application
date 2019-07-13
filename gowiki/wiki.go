bf0c5366a605471fcc3232f344fd5814c111f0bc
jding05 <jeffding05@gmail.com>
2019-07-12 17:31:40 -0700

    finish validation, template caching, error handling, saving pages, handling non-existent pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"html/template"
+	"regexp"
+	"errors"
 )
 
 // describes how page data will be stored in memory
@@ -28,41 +29,75 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl, ".html")
-	t.Execute(w, p)
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil // the title is t he second subexpression
 }
 
-// The function editHandler loads the page 
-// (or, if it doesn't exist, create an empty Page struct), and displays an HTML form.
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+// store into global var for template caching
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+// http.Error() sneds a specified HTTP response code and error message
+// template name is the template file name, so we must append ".html"
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
-		p = &Page{Title: title}
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
 	}
 	renderTemplate(w, "view", p)
 }
 
-// The function template.ParseFiles will read the contents of edit.html and
-//  return a *template.Template.
-
-// The method t.Execute executes the template, writing the generated HTML 
-// to the http.ResponseWriter. The .Title and .Body dotted identifiers 
-// refer to p.Title and p.Body.
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view"):]
-	p, _ := loadPage(title)
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return 
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
 
 // testing
 func main() {
 
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	// http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
